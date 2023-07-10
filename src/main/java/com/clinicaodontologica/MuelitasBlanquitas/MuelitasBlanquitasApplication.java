39c135f630c0eea2088220f7ae327c06e820d201
DavidGMont <davidgarmo@outlook.com>
2023-07-10 18:30:11 -0500

    🦷 Primer commit.

---
+++ src/main/java/com/clinicaodontologica/MuelitasBlanquitas/MuelitasBlanquitasApplication.java
@@ -0,0 +1,13 @@
+package com.clinicaodontologica.MuelitasBlanquitas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuelitasBlanquitasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuelitasBlanquitasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinicaodontologica/MuelitasBlanquitas/MuelitasBlanquitasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaodontologica.MuelitasBlanquitas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuelitasBlanquitasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
